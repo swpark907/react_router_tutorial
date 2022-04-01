caebd3b1330e5591519181fe10be30fc9143bfb3
swpark907 <76856321+swpark907@users.noreply.github.com>
2022-04-02 00:52:53 +0900

    update routing to each components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-import Item from './components/Item';
-import ItemDetail from './components/ItemDetail';
-import { Route, Router } from 'react-router-dom';
+import "./App.css";
+import Home from "./components/Home";
+import Item from "./components/Item";
+import ItemDetail from "./components/ItemDetail";
+import PageNotFound from "./components/PageNotFound";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+
+    <Routes>
+      <Route exact path="/" element={<Home />} />
+      <Route exact path="/item" element={<Item />} />
+      <Route path="/item/:id" element={<ItemDetail />} />
+      <Route element={<PageNotFound />} />
+    </Routes>
   );
 }
 
