3c53d789a0fcf2db90b7bf64942abb74ad1ae8a2
swpark907 <76856321+swpark907@users.noreply.github.com>
2022-04-03 23:25:07 +0900

    update sample data for item card

---
+++ src/assets/itemList.js
@@ -0,0 +1,35 @@
+export const itemList = [
+  {
+    id: 1,
+    name: 'item1',
+    imageURL: '',
+    price: 5000,
+    stock: 5,
+    desc: 'Lorem ipsum dolor sit amet consectetur.',
+  },
+  {
+    id: 2,
+    name: 'item2',
+    imageURL: '',
+    price: 2000,
+    stock: 9,
+    desc: 'Lorem ipsum dolor sit amet consectetur.',
+  },
+  {
+    id: 3,
+    name: 'item3',
+    imageURL: '',
+    price: 3000,
+    stock: 10,
+    desc: 'Lorem ipsum dolor sit amet consectetur.',
+  },
+  {
+    id: 4,
+    name: 'item4',
+    imageURL: '',
+    price: 4000,
+    stock: 11,
+    desc: 'Lorem ipsum dolor sit amet consectetur.',
+  },
+
+]
